80bf0719f4c13e54d8e07420fe42aa983a7529b3
Candela94 <candelafsg@alumnos.cei.es>
2025-06-23 17:23:32 +0200

    url

--- config/config.js
+++ config/config.js
@@ -1,30 +1,30 @@
 
-export const BACKEND_URL = process.env.BACKEND_URL || 'https://mode-backend.up.railway.app'
-export const PORT = process.env.PORT || '3003'
+export const BACKEND_URL = process.env.BACKEND_URL 
+export const PORT = process.env.PORT || 3000
 
-export const DB_USER =  process.env.DB_USER || "candelafsg"
-export const DB_PASS =  process.env.DB_PASS || "Juanotee77"
-export const CLUSTER =  process.env.DB_CLUSTER || "cei-practicas.k3hx5.mongodb.net"
-export const DATABASE = process.env.DATABASE ||  "mode"
+export const DB_USER =  process.env.DB_USER
+export const DB_PASS =  process.env.DB_PASS 
+export const CLUSTER =  process.env.DB_CLUSTER 
+export const DATABASE = process.env.DATABASE 
 
- export const JWT_SECRET = process.env.JWT_SECRET ||  "Et34WzL=fh5QoVw"
-
-
-export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || 'dnz96cick'
-
-export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || '123575681757184'
+ export const JWT_SECRET = process.env.JWT_SECRET 
 
 
-export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || 'Mff25ovm4HYl2aI1N2LA74qAGdQ'
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME 
+
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY
 
 
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET 
 
 
 
 
- export const CLOUDINARY_URL = process.env.CLOUDINARY_URL || "cloudinary://123575681757184:Mff25ovm4HYl2aI1N2LA74qAGdQ@dnz96cick"
-
- export const URL_LOCAL=process.env.URL_LOCAL || "http://localhost:3003"
 
 
- export const DB_MONGODB = process.env.DB_MONGODB || "mongodb+srv://candelafsg:Juanotee77@cei-practicas.k3hx5.mongodb.net/mode?retryWrites=true&w=majority&appName=CEI-PRACTICAS"
+ export const CLOUDINARY_URL = process.env.CLOUDINARY_URL 
+
+ export const URL_LOCAL=process.env.URL_LOCAL 
+
+
+ export const DB_MONGODB = process.env.DB_MONGODB 
